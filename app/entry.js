0d19b232caeec5b1e6bad7fe74b124f429b07f4e
yunosuke <h.yuunosuke@gmail.com>
2018-01-15 14:24:00 +0000

    changed setInterval

--- app/entry.js
+++ app/entry.js
@@ -2,23 +2,21 @@
 const $ = require('jquery');
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+const loadavg = $('#loadavg');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
 
-const movingButton = $('#moving-button');
-
 movingButton.click(() => {
   block.animate({ 'marginLeft': '500px' }, 500);
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
 
-const loadavg = $('#loadavg');
-
 setInterval(() => {
   $.get('/server-status', {}, (data) => {
     loadavg.text(data.loadavg.toString());
   });
-}, 10);
+}, 1000);
