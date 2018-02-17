385df2f3576ee02df57d087081d2f74e07fedcb9
masanorifunaki <funakimfunaki@gmail.com>
2018-02-17 04:37:51 +0000

    練習問題の実装

--- app/entry.js
+++ app/entry.js
@@ -4,15 +4,25 @@ const block = $('#block');
 const scalingButton = $('#scaling-button');
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
-  block.animate({ width: '100pt', height: '100pt' }, 2000);
+  block.animate({
+    width: '200pt',
+    height: '200pt'
+  }, 2000);
+  block.animate({
+    width: '100pt',
+    height: '100pt'
+  }, 2000);
 });
 
 const movingButton = $('#moving-button');
 
 movingButton.click(() => {
-  block.animate({ 'marginLeft': '500px' }, 500);
-  block.animate({ 'marginLeft': '20px' }, 1000);
+  block.animate({
+    'marginLeft': '500px'
+  }, 500);
+  block.animate({
+    'marginLeft': '20px'
+  }, 1000);
 });
 
 const loadavg = $('#loadavg');
@@ -21,4 +31,4 @@ setInterval(() => {
   $.get('/server-status', {}, (data) => {
     loadavg.text(data.loadavg.toString());
   });
-}, 10);
+}, 1000);
