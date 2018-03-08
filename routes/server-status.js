d20190a294393b044fcc3d2c0731e0cd307be0ca
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-03-08 06:30:36 +0000

    ロードアベレージを返す間隔を１秒ごとに変更

--- routes/server-status.js
+++ routes/server-status.js
@@ -1,10 +1,16 @@
+// 厳格モード
 'use strict';
+// routerを作成
 const express = require('express');
 const router = express.Router();
+// 'os'モジュールを作成
 const os = require('os');
 
+// GETアクセスが送られてきた場合の処理
 router.get('/', (req, res, next) => {
+  // JSON形式のオブジェクトをデータにセットして返す
+  // もしくはJSON形式でオブジェクトの内容を表示する
   res.json({ loadavg: os.loadavg() });
 });
-
+// このルーターをモジュールとして公開する
 module.exports = router;
