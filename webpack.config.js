abb8f059552d35ae02a5eee6581d21f39e276417
DDRAGON <davide.0829@gmail.com>
2018-03-16 17:24:24 +0900

    babel-loader 用の設定を追記 by 一括変換ツール

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,17 @@ module.exports = {
   output: {
     path: __dirname + '/public/javascripts',
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    }]
   }
 };
