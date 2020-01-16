ec9a9dd9ced6156cd44cad46eea75bca4cf2337e
SasuraiNoHoge <tanakanoobenjoygame@gmail.com>
2020-01-16 14:30:32 +0900

    レーダーチャートの実装

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,44 @@
 'use strict';
+//クライアントで常に実行される
 //2018年度のryjkmrさんのプルリクを参考にしました
 const BAR_HEIGHT = 400;
 import $ from 'jquery';
+var Chart = require('chart.js');
+var ctx = $('#myChart');
+var dos = $('#dos');
+let n = 1;
+
+var myRadarChart = new Chart(ctx, {
+  type: 'radar', 
+  data: { 
+      labels: ["1分毎", "5分毎", "15分毎"],
+      datasets: [{
+          label: 'サーバ負荷',
+          data: [0, 0, 0],
+          backgroundColor: 'RGBA(225,95,150, 0.5)',
+          borderColor: 'RGBA(225,95,150, 1)',
+          borderWidth: 1,
+          pointBackgroundColor: 'RGB(46,106,177)'
+      }]
+  },
+  options: {
+      title: {
+          display: true,
+          text: 'ロードアベレージ'
+      },
+      scale:{
+          ticks:{
+              suggestedMin: 0,
+              suggestedMax: 100,
+              stepSize: 10,
+              callback: function(value, index, values){
+                  return  value +  '%'
+              }
+          }
+      }
+  }
+});
+
 const block = $('#block');
 const scalingButton = $('#scaling-button');
 
@@ -28,7 +65,22 @@ setInterval(() => {
       num.text(array[index]); //テキストを書き換え
       let bar = $(`#bar-${index}`); //棒グラフオブジェクトを取得
       bar.height(BAR_HEIGHT * array[index]);
+      myRadarChart.data.datasets[0].data[index] = array[index]*100;
+      myRadarChart.update();
     });
     loadavg.text(data.loadavg.join(' : '));
   });
-}, 100);
+}, 1000);
+
+//見かけだけでほとんど負荷掛からなかったです
+dos.click(() => {
+  if(n < 2048)
+    n = n *2;
+});
+
+setInterval(() => {
+  for(let i = 0; i < n; i++){
+    console.log('負荷をかけます');
+  }
+},1);
+
