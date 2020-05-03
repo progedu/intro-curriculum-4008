348350ee10eeac28373efc453ecf87321bb83916
testori-2020-b2u <sypkc746wind@yahoo.co.jp>
2020-05-03 22:39:24 +0900

    ポーリングの間隔を1秒に変更

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var helmet = require('helmet');
-var session = require('express-session');
-var passport = require('passport');
-var GitHubStrategy = require('passport-github2').Strategy;
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+var session = require("express-session");
+var passport = require("passport");
+var GitHubStrategy = require("passport-github2").Strategy;
 
-var GITHUB_CLIENT_ID = 'f756acb8748f85e2014b';
-var GITHUB_CLIENT_SECRET = '0fc57f6660bd5da78873eeacda8c131859b64f30';
+var GITHUB_CLIENT_ID = "f756acb8748f85e2014b";
+var GITHUB_CLIENT_SECRET = "0fc57f6660bd5da78873eeacda8c131859b64f30";
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -19,68 +19,82 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
-},
-  function (accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-      return done(null, profile);
-    });
-  }
-));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    }
+  )
+);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var photosRouter = require('./routes/photos');
-var serverStatusRouter = require('./routes/server-status');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var photosRouter = require("./routes/photos");
+var serverStatusRouter = require("./routes/server-status");
 
 var app = express();
 app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({ secret: '417cce55dcfcfaeb', resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: "417cce55dcfcfaeb",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', ensureAuthenticated, usersRouter);
-app.use('/photos', photosRouter);
-app.use('/server-status', serverStatusRouter);
+app.use("/", indexRouter);
+app.use("/users", ensureAuthenticated, usersRouter);
+app.use("/photos", photosRouter);
+app.use("/server-status", serverStatusRouter);
 
-app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user:email'] }),
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  function (req, res) {}
+);
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
   function (req, res) {
-  });
+    res.redirect("/");
+  }
+);
 
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function (req, res) {
-    res.redirect('/');
-  });
-
-app.get('/login', function (req, res) {
-  res.render('login', { user: req.user });
+app.get("/login", function (req, res) {
+  res.render("login", { user: req.user });
 });
 
-app.get('/logout', function (req, res) {
+app.get("/logout", function (req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 // catch 404 and forward to error handler
@@ -92,11 +106,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
