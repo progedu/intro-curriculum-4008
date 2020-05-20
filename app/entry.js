6e445143ed2a675d36f332aa8f70fe2c0d2174f7
sonae556 <banana.sona@gmail.com>
2020-05-20 23:07:09 +0900

    サーバーへアクセスする間隔を1秒に変更

--- app/entry.js
+++ app/entry.js
@@ -1,24 +1,24 @@
-'use strict';
-import $ from 'jquery';
-const block = $('#block');
-const scalingButton = $('#scaling-button');
+"use strict";
+import $ from "jquery";
+const block = $("#block");
+const scalingButton = $("#scaling-button");
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
-  block.animate({ width: '100pt', height: '100pt' }, 2000);
+	block.animate({ width: "200pt", height: "200pt" }, 2000);
+	block.animate({ width: "100pt", height: "100pt" }, 2000);
 });
 
-const movingButton = $('#moving-button');
+const movingButton = $("#moving-button");
 
 movingButton.click(() => {
-  block.animate({ 'marginLeft': '500px' }, 500);
-  block.animate({ 'marginLeft': '20px' }, 1000);
+	block.animate({ marginLeft: "500px" }, 500);
+	block.animate({ marginLeft: "20px" }, 1000);
 });
 
-const loadavg = $('#loadavg');
+const loadavg = $("#loadavg");
 
 setInterval(() => {
-  $.get('/server-status', {}, (data) => {
-    loadavg.text(data.loadavg.toString());
-  });
-}, 10);
+	$.get("/server-status", {}, (data) => {
+		loadavg.text(data.loadavg.toString());
+	});
+}, 1000);
