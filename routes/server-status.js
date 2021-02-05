67d880cb120af9a585eb98139b3a30293c0ce143
rascasse-monaco <mktwkhr@gmail.com>
2021-02-05 19:58:28 +0900

    練習問題の回答

--- routes/server-status.js
+++ routes/server-status.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const os = require('os');
 
 router.get('/', (req, res, next) => {
-  res.json({ loadavg: os.loadavg() });
+  res.json({
+    loadavg: os.loadavg(),
+    cpuArch: os.arch(),//CPUのアーキテクチャ(ex: 'arm', 'arm64', 'x64', ...)
+    osPlatform: os.platform(),//OSの種類(ex: 'linux', 'win32', ...)
+    freeMemory: os.freemem() + ' Bytes'//空きメモリ容量
+  });
 });
 
 module.exports = router;
