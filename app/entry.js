5551ad951cfe729427d576531eeab0b49e7f46fa
heavygears <180gears@gmail.com>
2021-02-05 20:02:03 +0900

    ポーリングの感覚を1秒毎に変更

--- app/entry.js
+++ app/entry.js
@@ -3,14 +3,14 @@ import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
 
-scalingButton.click(() => {
+scalingButton.on('click', () => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
 
 const movingButton = $('#moving-button');
 
-movingButton.click(() => {
+movingButton.on('click', () => {
   block.animate({ 'marginLeft': '500px' }, 500);
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
@@ -21,4 +21,4 @@ setInterval(() => {
   $.get('/server-status', {}, (data) => {
     loadavg.text(data.loadavg.toString());
   });
-}, 10);
+}, 1000);
