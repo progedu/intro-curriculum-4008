ed6c7615047e44eaf5eca9627284d49634dd0d12
yu1k <48705423+yu1k@users.noreply.github.com>
2021-02-05 20:07:40 +0900

    Add

--- app/entry.js
+++ app/entry.js
@@ -3,22 +3,24 @@ import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
 
-scalingButton.click(() => {
+scalingButton.on("click", () => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
 
 const movingButton = $('#moving-button');
 
-movingButton.click(() => {
+movingButton.on("click", () => {
   block.animate({ 'marginLeft': '500px' }, 500);
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
 
 const loadavg = $('#loadavg');
 
-setInterval(() => {
+const loadavg_button = $("#loadavg-button");
+loadavg_button.on("click", () => {
   $.get('/server-status', {}, (data) => {
     loadavg.text(data.loadavg.toString());
   });
-}, 10);
+});
+
